5692cc47f3b812a3dbd3b7edfe4605dcd4fe8190
dhruv1206 <86611328+dhruv1206@users.noreply.github.com>
2021-10-08 08:41:53 +0530

    Create Nth Row Of Pascals Triangle in Python.py

---
+++ Nth Row Of Pascals Triangle in Python.py
@@ -0,0 +1,53 @@
+'''
+
+🇮🇳🇮🇳🇮🇳 BY~DHRUV AGRAWAL , INDIA 🇮🇳🇮🇳🇮🇳
+
+'''
+from math import factorial
+
+def combinations(total_num , chosen_num):
+    return int((factorial(total_num))/((factorial(chosen_num))*(factorial(total_num-chosen_num))))
+
+def nth_row_pascal(num_row):
+    my_list=[]
+    for i in range(num_row+1):
+        my_list.append(combinations(num_row , i))
+    return my_list
+def test_function(test_case):
+    n = test_case[0]
+    solution = test_case[1]
+    output = nth_row_pascal(n)
+    if solution == output:
+        print("Pass")
+    else:
+        print("Fail")
+
+n = 0
+solution = [1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 1
+solution = [1, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 2
+solution = [1, 2, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 3
+solution = [1, 3, 3, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 4
+solution = [1, 4, 6, 4, 1]
+
+test_case = [n, solution]
+test_function(test_case)
