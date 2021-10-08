88e654c05526e1d241ffb8fad9e84b654edd77b3
dhruv1206 <86611328+dhruv1206@users.noreply.github.com>
2021-10-08 08:07:09 +0530

    Create Duplicate Number in Python.py

---
+++ Duplicate Number in Python.py
@@ -0,0 +1,45 @@
+'''
+🇮🇳🇮🇳🇮🇳 BY~DHRUV AGRAWAL , INDIA 🇮🇳🇮🇳🇮🇳
+
+'''
+
+
+
+
+def duplicate_number(arr):
+    for i in arr:
+        if arr.count(i)==2:
+            return i 
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+    output = duplicate_number(arr)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+arr = [0, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 2, 3, 1, 4, 5, 3]
+solution = 3
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 1, 5, 4, 3, 2, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 1, 5, 5, 3, 2, 4]
+solution = 5
+
+test_case = [arr, solution]
+test_function(test_case)
