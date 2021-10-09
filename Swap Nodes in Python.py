cc0ca136ff29df33da56a003c0ba8ece05c2e044
dhruv1206 <86611328+dhruv1206@users.noreply.github.com>
2021-10-09 13:14:42 +0530

    Update Swap Nodes in Python.py

--- Swap Nodes in Python.py
+++ Swap Nodes in Python.py
@@ -4,8 +4,10 @@
 
 '''
 '''
-Problem Statement
+Problem Statement:
 Given a linked list, swap the two nodes present at position i and j. The positions are based on 0-based indexing.
+
+Note: You have to swap the nodes and not just the values.
 
 Example:
 
@@ -26,40 +28,42 @@ class Node(object):
         self.next=None
 
 def swap_nodes(head, left_index, right_index):
-    """
-    :param: head- head of input linked list
-    :param: left_index - indicates position
-    :param: right_index - indicates position
-    return: head of updated linked list with nodes swapped
-    TODO: complete this function and swap nodes present at left_index and right_index
-    Do not create a new linked list
-    """
-    # Values to swap
-    node = head
-    position = 0
-    while position <= right_index:
-        if position == left_index:
-            left_data = node.value
+    if left_index==right_index:
+        return
 
-        if position == right_index:
-            right_data = node.value
-        position += 1
-        node = node.next
-    
-    # Swapping values
-    node = head
-    position = 0
-    while position <= right_index:
-        if position == left_index:
-            node.value = right_data
+    prevX=None
+    currX=head
+    position_counter=0
+    while currX is not None and position_counter!=left_index:
+        prevX=currX
+        currX=currX.next
+        position_counter+=1 
 
-        if position == right_index:
-            node.value = left_data
-        position += 1
-        node = node.next
+    prevY=None
+    currY=head
+    position_counter=0
+    while currY is not None and position_counter!=right_index:
+        prevY=currY
+        currY=currY.next
+        position_counter+=1 
 
+    if currY is None or currX is None:
+        return
+
+    if prevX is not None:
+        prevX.next=currY
+    else:
+        head=currY
+
+    if prevY is not None:
+        prevY.next=currX
+    else:
+        head=currX
+
+    temp=currX.next
+    currX.next=currY.next
+    currY.next=temp
     return head
-
 
 def create_linked_list(arr):
     if len(arr)==0:
@@ -77,21 +81,69 @@ def print_linked_list(head):
         head = head.next
     print()
 
-arr = [3, 4, 5, 2, 6, 1, 9]
-left_index = 2 
-right_index = 4
-head = create_linked_list(arr)
-print_linked_list(swap_nodes(head,  left_index , right_index))
+################TESTING################
+def test_function(test_case):
+    head = test_case[0]
+    left_index = test_case[1]
+    right_index = test_case[2]
+    
+    left_node = None
+    right_node = None
+    
+    temp = head
+    index = 0
+    try:
+        while temp is not None:
+            if index == left_index:
+                left_node = temp
+            if index == right_index:
+                right_node = temp
+                break
+            index += 1
+            temp = temp.next
 
-arr = [3, 4, 5, 2, 6, 1, 9]
-left_index = 0
-right_index = 1
-head = create_linked_list(arr)
-print_linked_list(swap_nodes(head,  left_index , right_index))
+        updated_head = swap_nodes(head, left_index, right_index)
 
+        temp = updated_head
+        index = 0
+        pass_status = [False, False]
+
+        while temp is not None:
+            if index == left_index:
+                pass_status[0] = temp is right_node
+            if index == right_index:
+                pass_status[1] = temp is left_node
+
+            index += 1
+            temp = temp.next
+
+        if pass_status[0] and pass_status[1]:
+            print("Pass")
+        else:
+            print("Fail")
+        return updated_head
+    except Exception as e:
+        print("Fail")
 
 arr = [3, 4, 5, 2, 6, 1, 9]
 head = create_linked_list(arr)
 left_index = 3
 right_index = 4
-print_linked_list(swap_nodes(head,  left_index , right_index))
+
+test_case = [head, left_index, right_index]
+updated_head = test_function(test_case)
+
+arr = [3, 4, 5, 2, 6, 1, 9]
+left_index = 2 
+right_index = 4
+head = create_linked_list(arr)
+test_case = [head, left_index, right_index]
+updated_head = test_function(test_case)
+
+arr = [3, 4, 5, 2, 6, 1, 9]
+left_index = 0
+right_index = 1
+head = create_linked_list(arr)
+test_case = [head, left_index, right_index]
+updated_head = test_function(test_case)
+
