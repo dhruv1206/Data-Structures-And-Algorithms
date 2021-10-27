866436ebdf92752d394489e90ac28a3327c7aef0
dhruv1206 <86611328+dhruv1206@users.noreply.github.com>
2021-10-27 09:42:57 +0530

    Create KeyPad Combinations using Recursion in Python.py

---
+++ KeyPad Combinations using Recursion in Python.py
@@ -0,0 +1,66 @@
+'''
+🇮🇳🇮🇳🇮🇳 BY~DHRUV AGRAWAL , INDIA 🇮🇳🇮🇳🇮🇳
+'''
+
+
+def get_characters(num):
+    if num == 2:
+        return "abc"
+    elif num == 3:
+        return "def"
+    elif num == 4:
+        return "ghi"
+    elif num == 5:
+        return "jkl"
+    elif num == 6:
+        return "mno"
+    elif num == 7:
+        return "pqrs"
+    elif num == 8:
+        return "tuv"
+    elif num == 9:
+        return "wxyz"
+    else:
+        return ""
+
+def letterCombinations(num):
+    if num==0:
+        return [""]
+    elif num/10<1:
+        return [element for element in get_characters(num)]
+    else:
+        first=[elements for elements in get_characters(num%10)]
+        second=letterCombinations(num//10)
+        result=[]
+        for i in  first:
+            for j in  second:
+                result.append(j+i)
+    return result
+
+
+def test_keypad(input, expected_output):
+    if sorted(letterCombinations(input)) == expected_output:
+        print("Yay. We got it right.")
+    else:
+        print("Oops! That was incorrect.")
+
+input = 0
+expected_output = [""]
+test_keypad(input, expected_output)
+
+input = 23
+expected_output = sorted(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
+test_keypad(input, expected_output)
+
+input = 32
+expected_output = sorted(["da", "db", "dc", "ea", "eb", "ec", "fa", "fb", "fc"])
+test_keypad(input, expected_output)
+
+input = 8
+expected_output = sorted(["t", "u", "v"])
+test_keypad(input, expected_output)
+
+input = 354
+expected_output = sorted(["djg", "ejg", "fjg", "dkg", "ekg", "fkg", "dlg", "elg", "flg", "djh", "ejh", "fjh", "dkh", "ekh", "fkh", "dlh", "elh", "flh", "dji", "eji", "fji", "dki", "eki", "fki", "dli", "eli", "fli"])
+test_keypad(input, expected_output)
+
