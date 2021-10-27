ea3a66f0d5f37d6068b93a3af4656ccb57757c31
dhruv1206 <86611328+dhruv1206@users.noreply.github.com>
2021-10-27 14:07:05 +0530

    Create Deep Reverse using Recursion in Python.py

---
+++ Deep Reverse using Recursion in Python.py
@@ -0,0 +1,50 @@
+'''
+🇮🇳🇮🇳🇮🇳 BY~DHRUV AGRAWAL , INDIA 🇮🇳🇮🇳🇮🇳
+'''
+
+
+def deep_reverse(arr):
+
+    if type(arr) is not list:
+        return arr
+
+    else:
+        result=[]
+        arr=arr[::-1]
+        for item in arr:
+            result.append(deep_reverse(item))
+
+        return result
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+
+    output = deep_reverse(arr)
+    if output == solution:
+        print("Pass")
+    else:
+        print("False")
+
+arr = [1, 2, 3, 4, 5]
+solution = [5, 4, 3, 2, 1]
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [1, 2, [3, 4, 5], 4, 5]
+solution = [5, 4, [5, 4, 3], 2, 1]
+test_case = [arr, solution]
+test_function(test_case)
+
+
+arr = [1, [2, 3, [4, [5, 6]]]]
+solution = [[[[6, 5], 4], 3, 2], 1]
+test_case = [arr, solution]
+test_function(test_case)
+
+arr =  [1, [2,3], 4, [5,6]]
+solution = [ [6,5], 4, [3, 2], 1]
+test_case = [arr, solution]
+test_function(test_case)
+
+
